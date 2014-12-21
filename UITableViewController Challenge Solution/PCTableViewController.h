0fb04b4f065c77f628718063ea2ed7e018e7f20c
Paul Chin <paulchin@Pauls-MacBook-Air.local>
2014-12-21 13:37:13 +0800

    solved the challenge

--- UITableViewController Challenge Solution/PCTableViewController.h
+++ UITableViewController Challenge Solution/PCTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  PCTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Paul Chin on 12/21/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface PCTableViewController : UITableViewController
 
 @end
-
